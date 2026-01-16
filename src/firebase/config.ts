2c955b55b4a0237f25e44f6397de9a163d380e58
David Penny <david@whitelabeled.ca>
2026-01-16 16:54:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2120461843-5ad32",
+  "appId": "1:1015941417156:web:2b3a2fcc320cc94be16191",
+  "apiKey": "AIzaSyBCskj140uBcMCywLzqoPOG7dF7jtIsbn8",
+  "authDomain": "studio-2120461843-5ad32.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1015941417156"
+};
